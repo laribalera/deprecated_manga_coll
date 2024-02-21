fe7196df37d6aec971e38218f972dac34fd9bfcb
Larissa Balera <larissa.balera50@gmail.com>
2024-02-21 20:52:11 -0300

    major changes

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 
 # %%
 # Caminho para o arquivo CSV
-csv_file_path = 'volumes.csv'
+csv_volumes = 'volumes.csv'
+csv_colecoes = 'colecoes.csv'
 
 # Função para carregar os dados do CSV
-def load_data():
-    with open(csv_file_path, 'r') as csv_file:
+def load_data_volumes():
+    with open(csv_volumes, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = list(csv_reader)
+    return data
+
+def load_data_colecao():
+    with open(csv_colecoes, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         data = list(csv_reader)
     return data
@@ -19,7 +26,7 @@ def load_data():
 # %%
 # Função para salvar os dados no CSV
 def save_data(data):
-    with open(csv_file_path, 'w', newline='') as csv_file:
+    with open(csv_volumes, 'w', newline='') as csv_file:
         fieldnames = ['volume', 'titulo', 'author', 'status']
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         
@@ -30,13 +37,14 @@ def save_data(data):
 # %%
 # Rotas
 @app.route('/')
-def index():
-    data = load_data()
-    return render_template('index.html', data=data)
+def index(): 
+    volumes_data = load_data_volumes()
+    colecoes_data = load_data_colecao()
+    return render_template('index.html', volumes_data=volumes_data, colecoes_data=colecoes_data)
 
 @app.route('/add', methods=['POST'])
 def add():
-    data = load_data()
+    data = load_data_volumes()
     new_entry = {
         'volume': request.form['volume'],
         'titulo': request.form['titulo'],
@@ -49,7 +57,7 @@ def add():
 
 @app.route('/update/<int:row_id>', methods=['GET', 'POST'])
 def update(row_id):
-    data = load_data()
+    data = load_data_volumes()
     if request.method == 'POST':
         updated_entry = {
             'volume': request.form['volume'],
@@ -66,7 +74,7 @@ def update(row_id):
 
 @app.route('/delete/<int:row_id>')
 def delete(row_id):
-    data = load_data()
+    data = load_data_volumes()
     del data[row_id]
     save_data(data)
     return redirect(url_for('index'))
